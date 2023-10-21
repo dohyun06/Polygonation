55ee23e41bec1ff44c31df5fe121a37c05e44011
Your Name <hyun4you2006@naver.com>
2023-10-21 17:47:47 +0900

    edit project but bug is in it

--- triangle.js
+++ triangle.js
@@ -1,4 +1,5 @@
 import { Color } from './color.js';
+import { Dot } from './dot.js';
 
 export class Triangle {
   constructor(ctx, tmpCtx) {
@@ -29,25 +30,22 @@ export class Triangle {
     this.widthSize = this.stageWidth / this.divWidth;
     this.heightSize = this.stageHeight / this.divHeight;
 
-    for (let i = 0; i < this.blockNum; i++) {
-      this.points[i] = [
-        ((i % (this.divWidth + 2)) - 1) * this.widthSize + Math.floor(Math.random() * this.widthSize),
-        (Math.floor(i / (this.divWidth + 2)) - 1) * this.heightSize + Math.floor(Math.random() * this.heightSize),
-      ];
-    }
-    this.delaunay = d3.Delaunay.from(this.points);
-    for (let i = 0; ; i++) {
-      if (this.delaunay.trianglePolygon(i)[0][0] === undefined) break;
-
-      this.triangles[i] = this.delaunay.trianglePolygon(i);
-    }
-
     if (this.isLoaded) this.drawImage();
   }
 
   drawImage() {
     this.tmpCtx.drawImage(this.image, 0, 0, this.image.width, this.image.height, 0, 0, this.stageWidth, this.stageHeight);
     this.imgData = this.tmpCtx.getImageData(0, 0, this.stageWidth, this.stageHeight);
+
+    this.dot = new Dot(this.imgData, this.stageWidth, this.stageHeight);
+    this.points = dot.points;
+
+    this.delaunay = d3.Delaunay.from(this.points);
+    for (let i = 0; ; i++) {
+      if (this.delaunay.trianglePolygon(i)[0][0] === undefined) break;
+
+      this.triangles[i] = this.delaunay.trianglePolygon(i);
+    }
 
     for (let i = 0; i < this.triangles.length; i++) {
       this.color[i] = new Color(this.stageWidth, this.stageHeight, this.triangles[i], this.imgData);
@@ -70,7 +68,6 @@ export class Triangle {
       ctx.stroke();
     }
     */
-
     /*
     for (let i = 0; i < this.blockNum; i++) {
       ctx.beginPath();
@@ -81,18 +78,22 @@ export class Triangle {
     /*
     this.ctx.strokeStyle = '#FFF';
     */
+    /****************
     for (let i = 0; i < this.triangles.length; i++) {
       this.ctx.beginPath();
       this.ctx.moveTo(this.triangles[i][0][0], this.triangles[i][0][1]);
       this.ctx.lineTo(this.triangles[i][1][0], this.triangles[i][1][1]);
       this.ctx.lineTo(this.triangles[i][2][0], this.triangles[i][2][1]);
-      /*
+      ****************/
+    /*
       this.ctx.stroke();
       */
+    /*****************
       if (this.isLoaded === true) {
         this.ctx.fillStyle = `rgb(${this.color[i].color[0]}, ${this.color[i].color[1]}, ${this.color[i].color[2]})`;
         this.ctx.fill();
       }
     }
+    *****************/
   }
 }
